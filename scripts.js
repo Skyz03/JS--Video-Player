f40209380edb33391aa233c1a26afec3ff4c0a8f
Skyz03 <aakibdiscoo@gmail.com>
2022-02-05 20:10:19 +0545

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// GET THE ELEMENTS 
+const player = document.querySelector(".player");
+const video = player.querySelector('viewer');
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector("progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll('[date-skip]');
+const ranges = player.querySelectorAll('player__slider');
+
+// SETUP OUR FUNCTIONS
+
+// ADD THEM TO THE EVENT LISTENRS 
