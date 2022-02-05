7a17ba1ebaad244cdb21f8d7125241f5b47fe1c4
Skyz03 <aakibdiscoo@gmail.com>
2022-02-05 20:48:51 +0545

    Update till skip button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,33 @@
 // GET THE ELEMENTS 
 const player = document.querySelector(".player");
-const video = player.querySelector('viewer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector("progress__filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelectorAll('[date-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('player__slider');
 
 // SETUP OUR FUNCTIONS
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
 // ADD THEM TO THE EVENT LISTENRS 
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
